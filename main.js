0155589ef0377d94d629c8ac0064e4821b990865
Pedro Macedo Paronetto Faria Viana <pmpviana@gmail.com>
2024-05-15 14:47:11 -0300

    commit tarde 15/05/2024

---
+++ main.js
@@ -0,0 +1,39 @@
+function myFunction1() {
+    var x = document.getElementById("question1description");
+    var display = window.getComputedStyle(x).display;
+    if (display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function myFunction2() {
+    var x = document.getElementById("question2description");
+    var display = window.getComputedStyle(x).display;
+    if (display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function myFunction3() {
+    var x = document.getElementById("question3description");
+    var display = window.getComputedStyle(x).display;
+    if (display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function myFunction4() {
+    var x = document.getElementById("question4description");
+    var display = window.getComputedStyle(x).display;
+    if (display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
