2bdb6eb085a2010437ae5d0f0fae8e88989e1571
Pedro Macedo Paronetto Faria Viana <pmpviana@gmail.com>
2024-05-16 14:43:17 -0300

    final commit

--- main.js
+++ main.js
@@ -1,39 +1,63 @@
 function myFunction1() {
     var x = document.getElementById("question1description");
+    var y = document.getElementById("minusIcon1");
+    var z = document.getElementById("plusIcon1");
     var display = window.getComputedStyle(x).display;
     if (display === "none") {
         x.style.display = "block";
+        y.style.display = "block";
+        z.style.display = "none";
     } else {
         x.style.display = "none";
+        y.style.display = "none";
+        z.style.display = "block";
     }
 }
 
 function myFunction2() {
     var x = document.getElementById("question2description");
+    var y = document.getElementById("minusIcon2");
+    var z = document.getElementById("plusIcon2");
     var display = window.getComputedStyle(x).display;
     if (display === "none") {
         x.style.display = "block";
+        y.style.display = "block";
+        z.style.display = "none";
     } else {
         x.style.display = "none";
+        y.style.display = "none";
+        z.style.display = "block";
     }
 }
 
 function myFunction3() {
     var x = document.getElementById("question3description");
+    var y = document.getElementById("minusIcon3");
+    var z = document.getElementById("plusIcon3");
     var display = window.getComputedStyle(x).display;
     if (display === "none") {
         x.style.display = "block";
+        y.style.display = "block";
+        z.style.display = "none";
     } else {
         x.style.display = "none";
+        y.style.display = "none";
+        z.style.display = "block";
     }
 }
 
 function myFunction4() {
     var x = document.getElementById("question4description");
+    var y = document.getElementById("minusIcon4");
+    var z = document.getElementById("plusIcon4");
     var display = window.getComputedStyle(x).display;
     if (display === "none") {
         x.style.display = "block";
+        y.style.display = "block";
+        z.style.display = "none";
     } else {
         x.style.display = "none";
+        y.style.display = "none";
+        z.style.display = "block";
     }
 }
